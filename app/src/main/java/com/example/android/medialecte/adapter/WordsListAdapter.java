5a33d99539e53fdbe9f8c8ca8ae5297fb9c0c2b1
Dmidma <d.oussema.d@gmail.com>
2017-12-09 00:38:35 +0100

    Added an adapter

---
+++ app/src/main/java/com/example/android/medialecte/adapter/WordsListAdapter.java
@@ -0,0 +1,36 @@
+package com.example.android.medialecte.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by dmidma on 12/9/17.
+ */
+
+public class WordsListAdapter extends BaseAdapter {
+
+
+
+    
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
